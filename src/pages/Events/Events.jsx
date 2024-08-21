8a277dda0e5eda4dc6c61e1db79cf422ecbb8fb6
AlokPrasanna <alokprasanna1104@gmail.com>
2024-08-21 20:26:33 +0530

    Intergated backend with events

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb470482182968827ca1e2414d69feb31cc7f67b8b73453347e5f4f27ce51231
-size 3782
+oid sha256:bb7c39b4157a349eff4b1e0136f3a65895f3ee0fa79339b6940f96a944d0c99c
+size 3437
