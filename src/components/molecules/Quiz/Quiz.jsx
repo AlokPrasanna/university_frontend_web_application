a91815dc2b3cde95fdb8a66d8452adf90770011a
AlokPrasanna <alokprasanna1104@gmail.com>
2024-08-22 11:30:49 +0530

    Connected the backend with Quiz component

--- src/components/molecules/Quiz/Quiz.jsx
+++ src/components/molecules/Quiz/Quiz.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd434d9ea5a12c03031878c7faab67a5c8b519c312f8247038200f3f6643f636
-size 5999
+oid sha256:7db119fc43b8e5ccafcff1dea1d1e2568a64f057cc942e04f7d27434b5d52e91
+size 12147
